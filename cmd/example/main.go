c287362f16524872089d05ebb4402fc223e4ff3c
Konstantin Kukharev <i@kkukharev.ru>
2025-03-07 17:14:26 +0300

    update actions & example application

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"time"
 )
 
-const deadlineSeconds = 5
+const defaultLifetime = 5 * time.Second
+
+type Config struct {
+	Environment string
+	Lifetime    time.Duration
+}
 
 func main() {
+	c := new(Config)
+	flag.StringVar(&c.Environment, "env", "production", "runtime environment type")
+	flag.DurationVar(&c.Lifetime, "lt", defaultLifetime, "application lifetime")
+	flag.Parse()
+
 	t := time.NewTimer(time.Second)
 	ctx, cncl := context.WithDeadline(
 		context.Background(),
-		time.Now().Add(time.Second*deadlineSeconds))
+		time.Now().Add(c.Lifetime))
 	defer cncl()
 
 	for {
 		select {
 		case <-t.C:
-			fmt.Println("new tick")
+			fmt.Println("new tick in ", c.Environment)
 			t.Reset(time.Second)
 		case <-ctx.Done():
 			fmt.Println(ctx.Err())
